dab6f393b84a1ff46dc50244b1f998db98161d67
Mitchell Pung <mdpung@gmail.com>
2021-09-13 00:02:28 -0500

    Turned it into a functional state that uses React states to store data and uses 'fetch' to grab data from local files.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,31 +3,34 @@ import './App.css';
 
 
 function App() {
-  const [weaponCounts, setWeaponCounts] = React.useState(0);
+  const [primaryWeaponCount, setPrimaryWeaponCount] = React.useState('N/A');
+  const [secondaryWeaponCount, setSecondaryWeaponCount] = React.useState('N/A');
+  const [meleeWeaponCount, setMeleeWeaponCount] = React.useState('N/A');
+  const [archwingWeaponCount, setArchwingWeaponCount] = React.useState('N/A');
+  const [roboticWeaponCount, setRoboticWeaponCount] = React.useState('N/A');
 
-  const getWeaponCount = (weaponType, weaponTypePath) => {
+  const getWeaponCount = (weaponTypePath, action) => {
     fetch(weaponTypePath, {
       headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
       }
-    }).then(function (response) {
-      const json = response.json();
-      console.log(json);
-      const count = json.length;
+    }).then(response =>
+      response.json()
+    ).then(data => {
+      const count = data.length;
       console.log("Fetched weapon json of length " + count);
-      const counts = weaponCounts[weaponType] = count;
-      setWeaponCounts(counts);
+      // setWeaponCounts({ weaponType: count });
+      action(count);
     });
   }
 
   const getWeaponCounts = () => {
-    console.log("Primary stoage path: " + process.env.REACT_APP_PRIMARY_JSON_PRE_STORAGE_PATH);
-    getWeaponCount('primary', process.env.REACT_APP_PRIMARY_JSON_PRE_STORAGE_PATH);
-    getWeaponCount('secondary', process.env.REACT_APP_SECONDARY_JSON_PRE_STORAGE_PATH);
-    getWeaponCount('melee', process.env.REACT_APP_MELEE_JSON_PRE_STORAGE_PATH);
-    getWeaponCount('archwing', process.env.REACT_APP_ARCHWING_JSON_PRE_STORAGE_PATH);
-    getWeaponCount('robotic', process.env.REACT_APP_ROBOTIC_JSON_PRE_STORAGE_PATH);
+    getWeaponCount(process.env.REACT_APP_PRIMARY_JSON_PRE_STORAGE_PATH, count => setPrimaryWeaponCount(count));
+    getWeaponCount(process.env.REACT_APP_SECONDARY_JSON_PRE_STORAGE_PATH, count => setSecondaryWeaponCount(count));
+    getWeaponCount(process.env.REACT_APP_MELEE_JSON_PRE_STORAGE_PATH, count => setMeleeWeaponCount(count));
+    getWeaponCount(process.env.REACT_APP_ARCHWING_JSON_PRE_STORAGE_PATH, count => setArchwingWeaponCount(count));
+    getWeaponCount(process.env.REACT_APP_ROBOTIC_JSON_PRE_STORAGE_PATH, count => setRoboticWeaponCount(count));
   }
 
   return (
@@ -35,17 +38,23 @@ function App() {
       <header className="App-header">
         <button onClick={getWeaponCounts}>Get Weapon Counts</button>
         {
-          !weaponCounts || Object.keys(weaponCounts).length === 0
-            ? <p>No results</p>
-            : <div>
-              {Object.entries(weaponCounts).map(([key, value]) => {
-                return (
-                  <h3>
-                    {key} - {value}
-                  </h3>
-                )
-              })}
-            </div>
+          <div>
+            <h3>
+              Primary - {primaryWeaponCount}
+            </h3>
+            <h3>
+              Secondary - {secondaryWeaponCount}
+            </h3>
+            <h3>
+              Melee - {meleeWeaponCount}
+            </h3>
+            <h3>
+              Archwing - {archwingWeaponCount}
+            </h3>
+            <h3>
+              Robotic - {roboticWeaponCount}
+            </h3>
+          </div>
         }
       </header>
     </div>
